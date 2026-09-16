1804dff49a69d704be87baee0e3c003297289dda
Owen150 <michaeloduor50@gmail.com>
2026-09-16 22:04:54 +0300

    Initial Commit

---
+++ src/main/java/com/footballpredictor/splitpaymentapi/SplitPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.footballpredictor.splitpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitPaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/footballpredictor/splitpaymentapi/SplitPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballpredictor.splitpaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitPaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
