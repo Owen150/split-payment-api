8a62fe8e1a0c27393c9ce042f8d99022294f8f54
Owen150 <michaeloduor50@gmail.com>
2026-09-17 05:50:51 +0300

    Order Item Entity/Table

--- src/main/java/com/footballpredictor/splitpaymentapi/entity/OrderItem.java
+++ src/main/java/com/footballpredictor/splitpaymentapi/entity/OrderItem.java
@@ -1,4 +1,31 @@
 package com.footballpredictor.splitpaymentapi.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
 
+@Setter
+@Getter
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal unitPrice;
+
+    private BigDecimal subtotal;
+
+    // Getters and Setters - lombok will generate them automatically
 }
