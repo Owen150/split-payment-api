e968ed08fc47c10e573a958b9e8f34a05b3b768e
Owen150 <michaeloduor50@gmail.com>
2026-09-17 05:33:01 +0300

    Orders Entity/Table

--- src/main/java/com/footballpredictor/splitpaymentapi/entity/Order.java
+++ src/main/java/com/footballpredictor/splitpaymentapi/entity/Order.java
@@ -1,4 +1,34 @@
 package com.footballpredictor.splitpaymentapi.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+@Setter
+@Getter
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    private String currency = "kes";
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL
+    )
+    private List<OrderItem> items =
+            new ArrayList<>();
+
+    // Getters and Setters - lombok will generate them automatically
 }
---
+++ src/main/java/com/footballpredictor/splitpaymentapi/entity/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.footballpredictor.splitpaymentapi.entity;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PAID,
+    PAYMENT_FAILED,
+    CANCELLED,
+    FULFILLED
+}
