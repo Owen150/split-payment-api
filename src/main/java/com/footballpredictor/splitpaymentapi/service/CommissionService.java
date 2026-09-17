0daa9d75f874e6c489250b78cd1f7d2e6285334d
Owen150 <michaeloduor50@gmail.com>
2026-09-17 04:41:52 +0300

    Commission Service

---
+++ src/main/java/com/footballpredictor/splitpaymentapi/service/CommissionService.java
@@ -0,0 +1,22 @@
+package com.footballpredictor.splitpaymentapi.service;
+
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+public class CommissionService {
+    private static final BigDecimal RATE =
+            new BigDecimal("0.05");
+
+    public BigDecimal calculate(
+            BigDecimal orderTotal) {
+
+        return orderTotal
+                .multiply(RATE)
+                .setScale(
+                        2,
+                        RoundingMode.HALF_UP
+                );
+    }
+}
