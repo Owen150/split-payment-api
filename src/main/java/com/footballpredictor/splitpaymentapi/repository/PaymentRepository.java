a5967c16f1e55073da6813a4429d0e732743f86d
Owen150 <michaeloduor50@gmail.com>
2026-09-17 04:37:57 +0300

    Payment Repository

---
+++ src/main/java/com/footballpredictor/splitpaymentapi/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.footballpredictor.splitpaymentapi.repository;
+
+import com.footballpredictor.splitpaymentapi.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import org.hibernate.internal.util.Optional;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository
+    extends JpaRepository<Payment, Long> {
+    Optional<Payment>
+    findByStripeCheckoutSessionId(
+            String sessionId
+    );
+}
