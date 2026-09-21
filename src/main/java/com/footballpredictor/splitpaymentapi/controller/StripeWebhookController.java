ceb0fd8de00373951b6ccf7a5f62ff7b0813436b
Owen150 <michaeloduor50@gmail.com>
2026-09-21 20:18:33 +0300

    Stripe Webhook Controller

--- src/main/java/com/footballpredictor/splitpaymentapi/controller/StripeWebhookController.java
+++ src/main/java/com/footballpredictor/splitpaymentapi/controller/StripeWebhookController.java
@@ -1,4 +1,43 @@
 package com.footballpredictor.splitpaymentapi.controller;
 
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stripe")
 public class StripeWebhookController {
+
+    @Value("${stripe.webhook-secret}")
+    private String webhookSecret;
+
+    private final PaymentService paymentService;
+
+    public StripeWebhookController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<String> webhook(
+            @RequestBody String payload,
+            @RequestHeader("Stripe-Signature")
+            String signature
+    ) {
+        try {
+            Event event =
+                    Webhook.constructEvent(
+                            payload,
+                            signature,
+                            webhookSecret
+                    );
+            paymentService.processEvent(event);
+            return ResponseEntity.ok("Webhook received");
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Webhook failed");
+        }
+    }
 }
