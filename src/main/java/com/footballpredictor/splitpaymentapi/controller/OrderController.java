c7d938ac733898f0243fde47c863d38f583ff0eb
Owen150 <michaeloduor50@gmail.com>
2026-09-21 04:26:05 +0300

    Order Controller

--- src/main/java/com/footballpredictor/splitpaymentapi/controller/OrderController.java
+++ src/main/java/com/footballpredictor/splitpaymentapi/controller/OrderController.java
@@ -1,4 +1,23 @@
 package com.footballpredictor.splitpaymentapi.controller;
 
+import com.footballpredictor.splitpaymentapi.entity.Order;
+import com.footballpredictor.splitpaymentapi.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrder(@PathVariable Long id) {
+        return orderService.getOrder(id);
+    }
 }
