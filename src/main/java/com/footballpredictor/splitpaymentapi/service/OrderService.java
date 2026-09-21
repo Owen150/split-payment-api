2d1383be765cac519ad355484f404cc1c149d554
Owen150 <michaeloduor50@gmail.com>
2026-09-21 03:51:52 +0300

    Initialised order service for handling order-related operations

--- src/main/java/com/footballpredictor/splitpaymentapi/service/OrderService.java
+++ src/main/java/com/footballpredictor/splitpaymentapi/service/OrderService.java
@@ -1,4 +1,25 @@
 package com.footballpredictor.splitpaymentapi.service;
 
+import com.footballpredictor.splitpaymentapi.entity.Order;
+import com.footballpredictor.splitpaymentapi.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+    // Service class for handling order-related operations
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order getOrder(Long id) {
+        return orderRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new RuntimeException(
+                                "Order not found"
+                        )
+                );
+    }
 }
